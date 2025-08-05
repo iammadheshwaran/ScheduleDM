0042491821bd7ec44bbfc1e9c15b60715e6e1bd2
iammadheshwaran <m.madheshwaran21@gmail.com>
2025-08-05 21:12:25 +0530

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home"
 const App = () => {
   return (
      <>
-     <div className="min-h-screen flex flex-col justify-between bg-gray-50">
+     <div className="min-h-screen mx-3 flex flex-col justify-between bg-gray-50">
         <main className="flex-grow">
           <Home />
         </main>

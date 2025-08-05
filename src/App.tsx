18daa143497f12a7bb6338bfb0f3c2d94e4b544a
iammadheshwaran <m.madheshwaran21@gmail.com>
2025-08-05 20:13:56 +0530

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import Home from "./pages/Home"
 
 const App = () => {
   return (
-     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Home/>
-    </div>
+     <>
+     <div className="min-h-screen flex flex-col justify-between bg-gray-50">
+        <main className="flex-grow">
+          <Home />
+        </main>
+
+        <footer className="text-center text-sm text-gray-500 mt-10 pb-4">
+          Made with ❤️ by <a href="https://github.com/iammadheshwaran" target="_blank" className="underline hover:text-gray-700">Marsh</a>
+        </footer>
+
+        </div>
+     </>
   )
 }
 
